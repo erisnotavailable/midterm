a4211a19ded77de915929ee1ee221ff345e86372
Unknown <al6373@nyu.edu>
2020-10-06 14:19:25 -0400

    A super basic build with collide of objects, jump, etc.

---
+++ Assets/Sripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+    public bool isJumping;
+    public bool canJump = true;
+
+    public bool onGround;
+    float inputHorizontal;
+    float moveSpeed = 6f;
+    float jumpForce = 16f;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+            if (Input.GetButtonDown("Jump") && onGround == true){
+            isJumping = true;
+            }
+        
+    }
+
+    void FixedUpdate()
+    {
+        myRigidBody.velocity = new Vector2(inputHorizontal*moveSpeed, myRigidBody.velocity.y);
+        
+            if (isJumping) {
+            myRigidBody.velocity = new Vector2 (myRigidBody.velocity.x, jumpForce);
+            isJumping = false;
+            }     
+    }
+}
